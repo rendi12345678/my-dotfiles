a1270b493a5866b3e7edf2ca4d91021d0f700620
rendi12345678 <hardleberg@gmail.com>
2024-09-10 22:17:41 +0700

    Auto-update dotfiles: 2024-09-10 22:17:41

--- nvim/lua/rendi/core/keymaps.lua
+++ nvim/lua/rendi/core/keymaps.lua
@@ -2,83 +2,87 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
--- Move cursor left
+-- Cursor movements in insert mode
 keymap.set('i', '<C-h>', '<Left>', { noremap = true, silent = true, desc = 'Move cursor left' })
-
--- Move cursor right
 keymap.set('i', '<C-l>', '<Right>', { noremap = true, silent = true, desc = 'Move cursor right' })
 
--- Delete the character before the cursor
+-- Delete operations
 keymap.set('i', '<C-d>', '<Delete>', { noremap = true, silent = true, desc = 'Delete the character before the cursor' })
-
--- Delete the word before the cursor (Ctrl+w in Vim)
 keymap.set('i', '<C-w>', '<C-o>db<C-o>x', { noremap = true, silent = true, desc = 'Delete the word before the cursor' })
-
--- Delete the word after the cursor (Alt+d in Vim)
 keymap.set('i', '<C-D>', '<C-o>dw', { noremap = true, silent = true, desc = 'Delete the word after the cursor' })
 
+-- Clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit inset mode" })
+
+-- Exit insert mode with Ctrl-c
+keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" })
 
 -- Select all
-keymap.set("n", "<C-t>", "gg<S-v>G")
+keymap.set("n", "<C-t>", "gg<S-v>G", { desc = "Select all" })
 
--- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
+-- Keymaps that avoid affecting registers
+keymap.set("n", "x", '"_x', { noremap = true, silent = true, desc = "Delete without affecting register" })
+keymap.set("n", "<Leader>p", '""0"p', { noremap = true, silent = true, desc = "Paste from register 0" })
+keymap.set("n", "<Leader>P", '""0"P', { noremap = true, silent = true, desc = "Paste from register 0 before cursor" })
+keymap.set("v", "<Leader>p", '""0"p', { noremap = true, silent = true, desc = "Paste from register 0 in visual mode" })
+keymap.set("n", "<Leader>c", '"_c', { noremap = true, silent = true, desc = "Change without affecting register" })
+keymap.set("n", "<Leader>C", '"_C', { noremap = true, silent = true, desc = "Change to end of line without affecting register" })
+keymap.set("v", "<Leader>c", '"_c', { noremap = true, silent = true, desc = "Change in visual mode without affecting register" })
+keymap.set("v", "<Leader>C", '"_C', { noremap = true, silent = true, desc = "Change in visual mode to end of line" })
+keymap.set("n", "<Leader>d", '"_d', { noremap = true, silent = true, desc = "Delete without affecting register" })
+keymap.set("n", "<Leader>D", '"_D', { noremap = true, silent = true, desc = "Delete to end of line without affecting register" })
+keymap.set("v", "<Leader>d", '"_d', { noremap = true, silent = true, desc = "Delete in visual mode without affecting register" })
+keymap.set("v", "<Leader>D", '"_D', { noremap = true, silent = true, desc = "Delete to end of line in visual mode" })
 
-vim.keymap.set("n", "<leader>ld", "<cmd>LazyDocker<CR>", { desc = "Toggle LazyDocker", noremap = true, silent = true })
+-- Managing LazyDocker and Lazy.nvim
+keymap.set("n", "<leader>ld", "<cmd>LazyDocker<CR>", { noremap = true, silent = true, desc = "Toggle LazyDocker" })
+keymap.set("n", "<leader>ll", ":Lazy<CR>", { desc = "Open Lazy.nvim" })
 
--- increment/decrement numbers
-keymap.set("n", "+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- Increment and decrement numbers
+keymap.set("n", "+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "-", "<C-x>", { desc = "Decrement number" })
 
--- open Lazy nvim
-keymap.set("n", "<leader>ll", ":Lazy<CR>", { desc = "Open Lazy nvim" }) 
+-- Manage marks
+keymap.set('n', '<leader>ja', "'a", { noremap = true, silent = true, desc = "Jump to mark 'a'" })
+keymap.set('n', '<leader>jA', "'A", { noremap = true, silent = true, desc = "Jump to global mark 'A'" })
+keymap.set('n', '<leader>dm', ':delmarks a<CR>', { noremap = true, silent = true, desc = "Delete mark 'a'" })
+keymap.set('n', '<leader>dmA', ':delmarks A<CR>', { noremap = true, silent = true, desc = "Delete mark 'A'" })
+keymap.set('n', '<leader>lm', ':marks<CR>', { noremap = true, silent = true, desc = "List all marks" })
 
--- Jump to local mark
-keymap.set('n', '<leader>ja', "'a", { noremap = true, silent = true, desc = "Jump to Line of Mark 'a'" })
+-- Zen Mode toggle
+keymap.set("n", "<leader>zm", ":ZenMode<CR>", { desc = "Toggle Zen Mode" })
 
--- Jump to global mark
-keymap.set('n', '<leader>jA', "'A", { noremap = true, silent = true, desc = "Jump to Line of Global Mark 'A'" })
+-- Set wrap/nowrap
+keymap.set("n", "[w", ":set wrap<CR>", { desc = "Set wrap" })
+keymap.set("n", "]w", ":set nowrap<CR>", { desc = "Set nowrap" })
 
--- Delete mark 'a'
-keymap.set('n', '<leader>dm', ':delmarks a<CR>', { noremap = true, silent = true, desc = "Delete Mark 'a'" })
+-- Window management
+keymap.set("n", "<leader>sw", "<C-w>w", { desc = "Switch window" })
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Equalize window sizes" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
--- Delete mark 'A'
-keymap.set('n', '<leader>dm', ':delmarks A<CR>', { noremap = true, silent = true, desc = "Delete Mark 'a'" })
+-- Tab management
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- List all marks
-keymap.set('n', '<leader>lm', ':marks<CR>', { noremap = true, silent = true, desc = "List All Marks" })
+-- Custom substitute throughout the entire file
+keymap.set('n', '<leader>ss', [[:%s/<C-r>=input("Find: ")<CR>/<C-r>=input("Replace with: ")<CR>/g<CR>]], 
+    { noremap = true, silent = false, desc = "Substitute throughout the entire file" })
 
--- Toggle Zen Mode 
-keymap.set("n", "<leader>zm", ":ZenMode<CR>", { desc = "Toggle Zen Mode" }) 
+-- Custom substitute on the current line only
+keymap.set('n', '<leader>sl', [[:s/<C-r>=input("Find: ")<CR>/<C-r>=input("Replace with: ")<CR>/g<CR>]], 
+    { noremap = true, silent = false, desc = "Substitute on the current line" })
 
--- Set wrap and nowrap
-keymap.set("n", "[w", ":set wrap<cr>", { desc = "Set wrap" })
-keymap.set("n", "]w", ":set nowrap<cr>", { desc = "Set nowrap" })
+-- Custom substitute within the selected visual range
+keymap.set('v', '<leader>sV', [[:s/<C-r>=input("Find: ")<CR>/<C-r>=input("Replace with: ")<CR>/g<CR>]], 
+    { noremap = true, silent = false, desc = "Substitute in visual selection" })
 
--- switch window
-keymap.set("n", "<leader>sw", "<C-w>w", { desc = "Decrement number" }) -- decrement
+-- Custom substitute for the word under the cursor
+keymap.set('n', '<leader>sc', [[:%s/\<<C-r><C-w>\>/<C-r>=input("Replace with: ")<CR>/gI<CR>]], 
+    { noremap = true, silent = false, desc = "Substitute word under cursor" })
 
--- window managemenEsct
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
-
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
