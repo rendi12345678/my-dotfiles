356fe0a9e9369205f9985bde389f13a2fb04c8c2
rendi12345678 <hardleberg@gmail.com>
2024-09-11 08:37:54 +0700

    Auto-update dotfiles: 2024-09-11 08:37:54

--- nvim/lua/rendi/plugins/coq.lua
+++ nvim/lua/rendi/plugins/coq.lua
@@ -48,6 +48,7 @@ return {
     lsp.ts_ls.setup(coq.lsp_ensure_capabilities{})
     lsp.tailwindcss.setup(coq.lsp_ensure_capabilities{})
     lsp.emmet_ls.setup(coq.lsp_ensure_capabilities{})
+    lsp.jsonls.setup(coq.lsp_ensure_capabilities{})
     lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities{
       settings = {
       ["rust-analyzer"] = {
